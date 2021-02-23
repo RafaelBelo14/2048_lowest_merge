aa800d8786111d8016aad371a8486b25f4aea741
Rafael Baptista <rafael14belo@gmail.com>
2021-02-23 22:13:22 +0000

    Versão mais simples mas ainda com erros

--- 2048.cpp
+++ 2048.cpp
@@ -1,59 +1,50 @@
 #include <iostream>
 using namespace std;
 
-int check_move(int before_tile, int tile, int next_tile);
+int check_move(int tile, int next_tile);
 
-
-int main() {
+int main()
+{
   int reps, size, moves;
   int modifier, aux1, aux2;
 
   cin >> reps;
 
-  for (int i = 0; i < reps; i++) {
+  for (int i = 0; i < reps; i++)
+  {
     cin >> size;
     cin >> moves;
     int matriz[size][size];
 
-    for(int j = 0; j < size; j++) {
-      for (int k = 0; k < size; k++) {
+    for (int j = 0; j < size; j++)
+    {
+      for (int k = 0; k < size; k++)
+      {
         cin >> matriz[j][k];
       }
     }
 
-    // slide right
+    // slide right        
 
-    for(int j = 0; j < size; j++) {
-      for (int k = 0; k < size; k++) {
+    for (int j = 0; j < size; j++)
+    {
+      for (int k = 0; k < size; k++)
+      {
+        if (k != size - 1)
+        {
+          modifier = check_move(matriz[j][k], matriz[j][k + 1]);
 
-        if (k != size - 1 && k > 0) {
-          modifier = check_move(matriz[j][k - 1], matriz[j][k], matriz[j][k + 1]);
-
-          if (modifier == 0) { // tiles iguais e ainda não houve soma
-            aux1 = matriz[j][k - 1];
-            matriz[j][k] += aux1;
-            matriz[j][k - 1] = 0;
-          }
-          else if (modifier == 1) {
-            aux1 = matriz[j][k - 1];
-            matriz[j][k - 1] = 0;
-            matriz[j][k] = aux1;
+          if (modifier == 1)
+          {
+            matriz[j][k] = 0;
             matriz[j][k + 1] *= 2;
-            ++k;
           }
 
-          else if (modifier == -1) {
-            aux1 = matriz[j][k - 1];
-            aux2 = matriz[j][k];
-            matriz[j][k - 1] = 0;
-            matriz[j][k] = aux1;
-            matriz[j][k + 1] = aux2;
-          }
-
-          else if (modifier == -2){
-            aux1 = matriz[j][k - 1];
-            matriz[j][k] = aux1;
-            matriz[j][k - 1] = 0;
+          else if (modifier == -1)
+          {
+            aux1 = matriz[j][k];
+            matriz[j][k] = 0;
+            matriz[j][k + 1] = aux1;
           }
         }
       }
@@ -61,8 +52,10 @@ int main() {
 
     cout << "================" << endl;
 
-    for(int j = 0; j < size; j++) {
-      for (int k = 0; k < size; k++) {
+    for (int j = 0; j < size; j++)
+    {
+      for (int k = 0; k < size; k++)
+      {
         cout << matriz[j][k] << " ";
       }
 
@@ -73,21 +66,18 @@ int main() {
   return 0;
 }
 
-int check_move(int before_tile, int tile, int next_tile) {
-  if (before_tile == tile) {
+int check_move(int tile, int next_tile)
+{
+  if (tile == next_tile)
+  {
+    return 1;
+  }
+  else if (next_tile == 0)
+  {
+    return -1;
+  }
+  else
+  {
     return 0;
   }
-  else {
-      if (tile == next_tile) {
-        return 1;
-      }
-      else if (next_tile == 0) {
-        return -1;
-      }
-      else if (next_tile != -1){
-        return -2;
-      }
-  }
-
-  return -3;
 }
