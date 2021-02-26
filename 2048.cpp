a421b70171b9d6086af1c0352c969f5530ce68de
Rafael Baptista <rafael14belo@gmail.com>
2021-02-26 00:27:15 +0000

    Slide Left and Right

--- 2048.cpp
+++ 2048.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
-int check_move(int tile, int next_tile);
+void slide_right(int line[], int size, int flag);
+void push_line_right(int line[], int index);
+
+void slide_left(int line[], int size, int flag);
+void push_line_left(int line[], int index, int size);
+
+void slide_up(int line[], int size, int flag);
+void push_line_up(int line[], int index, int size);
+
+void slide_down(int line[], int size, int flag);
+void push_line_down(int line[], int index, int size);
+
+void print_line(int line[], int size);
 
 int main()
 {
   int reps, size, moves;
-  int modifier, aux1, aux2;
-
+  int flag = 0;
+  int slide_help_iterator = 0;
   cin >> reps;
 
   for (int i = 0; i < reps; i++)
@@ -24,31 +37,9 @@ int main()
       }
     }
 
-    // slide right        
-
     for (int j = 0; j < size; j++)
     {
-      for (int k = 0; k < size; k++)
-      {
-        if (k != size - 1)
-        {
-          modifier = check_move(matriz[j][k], matriz[j][k + 1]);
-
-          if (modifier == 1)
-          {
-            matriz[j][k] = 0;
-            matriz[j][k + 1] *= 2;
-            
-          }
-
-          else if (modifier == -1)
-          {
-            aux1 = matriz[j][k];
-            matriz[j][k] = 0;
-            matriz[j][k + 1] = aux1;
-          }
-        }
-      }
+      slide_left(matriz[j], size, flag);
     }
 
     cout << "================" << endl;
@@ -67,18 +58,183 @@ int main()
   return 0;
 }
 
-int check_move(int tile, int next_tile)
+void slide_right(int line[], int size, int flag)
 {
-  if (tile == next_tile)
+
+  for (int i = 0; i < size - 1; i++)
   {
-    return 1;
+    if (line[i] == line[i + 1] && flag == 0)
+    {
+      line[i + 1] *= 2;
+      push_line_right(line, i);
+      flag = 1;
+    }
+
+    else if (line[i] == line[i + 1] && flag == 1)
+    {
+      flag = 0;
+    }
+
+    else if (line[i + 1] == 0)
+    {
+      push_line_right(line, i + 1);
+    }
+
+    else
+    {
+      flag = 0;
+    }
+
+    //print_line(line, size);
   }
-  else if (next_tile == 0)
+}
+
+void slide_left(int line[], int size, int flag)
+{
+
+  for (int i = size - 1; i > 0; i--)
   {
-    return -1;
+    if (line[i] == line[i - 1] && flag == 0)
+    {
+      line[i - 1] *= 2;
+      push_line_left(line, i, size);
+      flag = 1;
+    }
+
+    else if (line[i] == line[i - 1] && flag == 1)
+    {
+      flag = 0;
+    }
+
+    else if (line[i - 1] == 0)
+    {
+      push_line_left(line, i - 1, size);
+    }
+
+    else
+    {
+      flag = 0;
+    }
+
+    //print_line(line, size);
+  }
+}
+
+/*
+void slide_up(int line[], int size, int flag)
+{
+
+  for (int i = 0; i < size - 1; i++)
+  {
+    if (line[i] == line[i + 1] && flag == 0)
+    {
+      line[i + 1] *= 2;
+      push_line(line, i);
+      flag = 1;
+    }
+
+    else if (line[i] == line[i + 1] && flag == 1)
+    {
+      flag = 0;
+    }
+
+    else if (line[i + 1] == 0)
+    {
+      push_line(line, i + 1);
+    }
+
+    else {
+      flag = 0;
+    }
+
+    //print_line(line, size);
+  }
+}
+
+void slide_down(int line[], int size, int flag)
+{
+
+  for (int i = 0; i < size - 1; i++)
+  {
+    if (line[i] == line[i + 1] && flag == 0)
+    {
+      line[i + 1] *= 2;
+      push_line(line, i);
+      flag = 1;
+    }
+
+    else if (line[i] == line[i + 1] && flag == 1)
+    {
+      flag = 0;
+    }
+
+    else if (line[i + 1] == 0)
+    {
+      push_line(line, i + 1);
+    }
+
+    else {
+      flag = 0;
+    }
+
+    //print_line(line, size);
+  }
+}
+*/
+
+void push_line_right(int line[], int index)
+{
+  /*if (index == 0)
+  {
+    line[0] = 0;
+    return;
   }
   else
+  {*/
+  for (int i = index; i > 0; i--)
   {
-    return 0;
+    line[i] = line[i - 1]; //move all element to the left except first one
   }
+  line[0] = 0;
+  //}
+}
+
+void push_line_left(int line[], int index, int size)
+{
+  for (int i = index; i < size; i++)
+  {
+    line[i] = line[i + 1]; //move all element to the left except first one
+  }
+  line[size - 1] = 0;
+}
+
+/*
+void push_line_up(int line[], int index, int size)
+{
+  for (int i = index; i < size; i++)
+  {
+    line[i] = line[i + 1]; //move all element to the left except first one
+  }
+  line[size - 1] = 0;
+}
+
+void push_line_down(int line[], int index, int size)
+{
+  for (int i = index; i < size; i++)
+  {
+    line[i] = line[i + 1]; //move all element to the left except first one
+  }
+  line[size - 1] = 0;
+}
+*/
+
+void print_line(int line[], int size)
+{
+  cout << "........" << endl;
+  for (int i = 0; i < size; i++)
+  {
+    cout << line[i] << " ";
+  }
+
+  cout << endl;
 }
